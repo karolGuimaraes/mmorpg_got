d89027153358af70590e6cb8d041aabdc3f34484
Karolinny Guimaraes <karolinny.silva@gmail.com>
2022-09-26 11:05:15 -0300

    Form validation

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,22 @@
 module.exports.cadastro = function (application, req, res) {
-    res.render('cadastro');
-}
+    res.render('cadastro', {validacao: null, dados: null});
+};
+
+
+module.exports.cadastrar = function (application, req, res) {
+    var dados = req.body;
+
+    req.assert('nome', 'Nome é obrigatório.').notEmpty();
+    req.assert('usuario', 'Usuário é obrigatório.').notEmpty();
+    req.assert('senha', 'Informe uma senha.').notEmpty();
+    req.assert('casa', 'Selecione uma casa.').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros) {
+        res.render('cadastro', {validacao: erros, dados: dados});
+        return
+    }
+
+    res.send('Cadastrado');
+};
