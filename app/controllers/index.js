448515401b0eb42f9c35309f5d2ee0f653fb9cac
Karolinny Guimaraes <karolinny.silva@gmail.com>
2022-09-27 11:49:58 -0300

    login controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.main = function (application, req, res) {
-    res.render('index');
+    res.render('index', {validacao: null, dados: null});
+}
+
+module.exports.autenticar = function (application, req, res) {
+    const dados = req.body;
+    console.log(dados)
+
+    req.assert('usuario', 'Usuário é obrigatório').notEmpty();
+    req.assert('senha', 'Senha é obrigatória').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros) {
+        res.render('index', {validacao: erros, dados: dados});
+        return
+    }
+
+    res.send('Logado');
 }
