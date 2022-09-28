b4cb1f50b8050736f82962b0549bde6164ae17f4
Karolinny Guimaraes <karolinny.silva@gmail.com>
2022-09-28 08:15:39 -0300

    middleware express-session

--- config/server.js
+++ config/server.js
@@ -10,6 +10,9 @@ var bodyParser = require('body-parser');
 /* importar o módulo do express-validator */
 var expressValidator = require('express-validator');
 
+/* importar o módulo do express-session */
+var expressSession = require('express-session');
+
 /* iniciar o objeto do express */
 var app = express();
 
@@ -25,6 +28,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 /* configurar o middleware express-validator */
 app.use(expressValidator());
+
+/* configurar o middleware express-session */
+/*
+secret: ID da sessão
+resave: Se true, a cada request do usuário grava a sessão no servidor,
+mesmo que não tenha alterações.
+saveUninitialized: Se true, criará uma sessão nova quando a atual for atualizada
+*/
+app.use(expressSession({
+	secret: 'got',
+	resave: false,
+	saveUninitialized: false
+}));
 
 /* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign()
